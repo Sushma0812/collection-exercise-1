fc7a48fd0e47d470a9c01d77b130b647892af72a
Sushma Bhupathi <bhupathisushma1998@gmail.com>
2020-09-08 03:59:23 +0000

    added patient details program

--- Program-6/src/com/stackroute/Patient.java
+++ Program-6/src/com/stackroute/Patient.java
@@ -1,31 +1,34 @@
 package com.stackroute;
-
 import java.util.*;
-
+//this class contains a parameterized constructor,setters and getters methods
 class Patient {
     int registrationNumber;
     String name;
-    String gender;
+    char gender;
 
-public Patient(int registrationNumber, String name, char gender) {
+    public Patient(int registrationNumber, String name, char gender) {
         this.registrationNumber = registrationNumber;
         this.name = name;
         this.gender = gender;
     }
 
     public void setRegistrationNumber(int registrationNumber) {
+
         this.registrationNumber = registrationNumber;
     }
 
     public int getRegistrationNumber() {
+
         return registrationNumber;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public String getName() {
+
         return name;
     }
 
@@ -35,6 +38,18 @@ public Patient(int registrationNumber, String name, char gender) {
 
     public char getGender() {
         return gender;
+    }
+
+    public int hashCode() {
+        return registrationNumber;
+    }
+
+    public String toString() {
+        return "[" + registrationNumber + ", " + name + ", " + gender + "]";
+    }
+    //
+    public int compareTo(Patient patientDetails) {
+        return (this.getName().compareTo(patientDetails.getName()));
     }
 
 
--- Program-6/src/com/stackroute/PatientService.java
+++ Program-6/src/com/stackroute/PatientService.java
@@ -1,36 +1,40 @@
 package com.stackroute;
-
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class PatientService {
-    //this method is used to add patient details
-    public static Set<Patient> addPatient(int registrationNumber, String name, String gender) {
-        HashSet<Patient> set = new HashSet<>();
-        set.add(new Patient(101,"sushma",'f'));
-        set.add(new Patient(102,"Bhavana",'f'));
-        System.out.println(set);
+    //this method is used to add patient details and display the details
+    public static HashSet<Patient> addPatient(int registrationNumber, String name, char gender) {
+        HashSet<Patient> set = new HashSet<Patient>();
+        set.add(new Patient(101, "Andreia", 'f'));
+        set.add(new Patient(102, "Jyothi", 'f'));
+        set.add(new Patient(103, "Tom", 'm'));
+        set.add(new Patient(104, "John", 'm'));
+        Iterator<Patient> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            Patient patient = (Patient) iterator.next();
+            System.out.println(patient);
+        }
         return set;
     }
-
-    //this method is used to search whether patient is present or not using registration number
-    public static Set<Patient> searchPatient( int registrationNumber) {
-
+    //this method is used to check whether the patient is present or not
+    public static int searchPatient(Set<Patient> set, int registrationNumber) {
+        if (set.contains(registrationNumber)) {
+            System.out.println("Present");
+        } else
+            System.out.println("not Present");
+        return 0;
     }
-
-    //this method is used to sort the patient names by using gender
-    public static void getAllPatients(char gender) {
-        if (set.contains(gender)) {
-            System.out.println(set);
-
-        }
+    //this method is used to get the sorted patient details using gender
+    public static int getAllPatientsByGender(Set<Patient> set, char gender) {
+        List<Patient> list = new ArrayList<Patient>(set);
+        Set<Patient> patientDetails=new TreeSet<Patient>(Comparator.comparing(Patient::gender));
+        Collections.sort(list);
+        System.out.println("After Sorting:" + list);
+        return 0;
     }
-
     public static void main(String[] args) {
-        Set<Patient> patient = new HashSet<Patient>();
-        Set<Patient> patient = new HashSet<Patient>();
-        set= addPatient();
-        searchPatient(set,101);
-        getAllPatient('F');
+        Set<Patient> set = addPatient(105, "Lavanya", 'f');
+        searchPatient(set, 101);
+        getAllPatientsByGender(set, 'f');
     }
 }
